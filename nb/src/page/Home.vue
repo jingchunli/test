45bd28e4e1fc307119b76ff991e21b23a6100c81
jingchun <1135050252@qq.com>
2021-06-18 16:47:09 +0800

    格式化

--- nb/src/page/Home.vue
+++ nb/src/page/Home.vue
@@ -4,28 +4,28 @@
     <div>{{nowTime}}</div>
     1
     <router-link to="/View">index</router-link>
-    <router-view></router-view> 
+    <router-view></router-view>
   </div>
 </template>
 <script>
 export default {
-  name: "home",
-  data() {
+  name: 'home',
+  data () {
     return {
       nowTime: Date()
-    };
+    }
   },
   methods: {},
-  mounted() {
-    let _this = this; // 声明一个变量指向Vue实例this，保证作用域一致
+  mounted () {
+    let _this = this // 声明一个变量指向Vue实例this，保证作用域一致
     this.timer = setInterval(() => {
-      _this.nowTime = new Date().toLocaleString(); // 修改数据date
-    }, 1000);
+      _this.nowTime = new Date().toLocaleString() // 修改数据date
+    }, 1000)
   },
-  beforeDestroy() {
+  beforeDestroy () {
     if (this.timer) {
-      clearInterval(this.timer); // 在Vue实例销毁前，清除我们的定时器
+      clearInterval(this.timer) // 在Vue实例销毁前，清除我们的定时器
     }
   }
-};
+}
 </script>
